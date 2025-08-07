4dd5a0f9cb6e02ff20d8656eb3bea08b574dfda6
daganeabdul <daganeabdulrahman@gmail.com>
2025-08-07 14:51:21 +0300

    js function to python syntax

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print(f"Hello, programmer!")
+greet_programmer()    
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+greet_with_default()
+greet_with_default("sunny")    
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+sum = add(45,55)
+print(f"{sum}")
 
 def halve(number):
-    pass
+   if not isinstance(number, (int, float)):
+       return None
+   return number/2
+   
+print(f"{halve(4)}")
+print(f"two")
